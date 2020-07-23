f9db4755142c40bcb74057d46b4994a19476bbe9
Ubuntu <ubuntu@ip-172-31-58-122.ec2.internal>
2020-07-23 11:00:37 +0000

    root permission for cwagent and unzip installation

--- install_CWagent.sh
+++ install_CWagent.sh
@@ -1,6 +1,7 @@
+sudo apt install -y unzip
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
 unzip AmazonCloudWatchAgent.zip
 sudo ./install.sh
 sudo cp config.json /opt/aws/amazon-cloudwatch-agent/bin
 cd /opt/aws/amazon-cloudwatch-agent/bin
-./amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json  -s
+sudo ./amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json  -s
